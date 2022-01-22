4e6640e05d0cf30c22cfc49134a18bbc87e56a03
Vivek Pratap Singh <vivekpratapsingh@Viveks-MacBook-Pro.local>
2022-01-22 12:53:04 +0530

    Added the flat list element successfully

---
+++ src/screens/ListScreen.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {View, Text, StyleSheet, FlatList} from 'react-native';
+
+
+const ListScreen = () => {
+    const friends = [
+          {name: 'friend #1'},
+          {name: 'friend #2'},
+          {name: 'friend #3'},
+          {name: 'friend #4'},
+          {name: 'friend #5'},  
+          {name: 'friend #6'},
+          {name: 'friend #7'},
+          {name: 'friend #8'},
+          {name: 'friend #9'},
+    ];
+    
+    return (
+        <FlatList
+        data={friends}
+        renderItem={
+            ({item}) =>{
+                return <Text>{item.name}</Text>
+            }
+        }
+        />
+    );
+};
+
+const styles = StyleSheet.create({
+    textStyle: {
+        fontSize: 20
+    }
+});
+
+export default ListScreen;
