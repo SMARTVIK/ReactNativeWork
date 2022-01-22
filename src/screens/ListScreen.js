e98e9d6822b9d21a3595628b55837467232db171
Vivek Pratap Singh <vivekpratapsingh@Viveks-MacBook-Pro.local>
2022-01-22 13:00:25 +0530

    Now rendering the list with key extractor

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -4,19 +4,20 @@ import {View, Text, StyleSheet, FlatList} from 'react-native';
 
 const ListScreen = () => {
     const friends = [
-          {name: 'friend #1', key:'1'},
-          {name: 'friend #2', key:'2'},
-          {name: 'friend #3', key:'3'},
-          {name: 'friend #4', key:'4'},
-          {name: 'friend #5', key:'5'},  
-          {name: 'friend #6', key:'6'},
-          {name: 'friend #7', key:'7'},
-          {name: 'friend #8', key:'8'},
-          {name: 'friend #9', key:'9'},
+          {name: 'friend #1'},
+          {name: 'friend #2'},
+          {name: 'friend #3'},
+          {name: 'friend #4'},
+          {name: 'friend #5'},  
+          {name: 'friend #6'},
+          {name: 'friend #7'},
+          {name: 'friend #8'},
+          {name: 'friend #9'},
     ];
     
     return (
         <FlatList
+        keyExtractor={(friend) => friend.name}
         data={friends}
         renderItem={
             ({item}) =>{
