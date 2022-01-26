b89ca455a89a1105b5a29f7658a8e21489249e87
smartvik <vps631532@gmail.com>
2022-01-27 00:47:37 +0530

    TextInput screen done

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,59 +1,64 @@
-import React from 'react';
-import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
+import React from 'react'
+import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native'
 
 const HomeScreen = ({ navigation }) => {
-  return <View>
-    <Button
-      onPress={
-        () => {
-          navigation.navigate("Components")
-        }
-      }
-      title="Go to components demo" />
+  return (
+    <View>
+      <Button
+        onPress={() => {
+          navigation.navigate('Components')
+        }}
+        title="Go to components demo"
+      />
 
-    <Button
-      onPress={() => {
-        navigation.navigate('List')
-      }
-      }
-      title="Go to List screen" />
+      <Button
+        onPress={() => {
+          navigation.navigate('List')
+        }}
+        title="Go to List screen"
+      />
 
-    <Button
-      onPress={() => {
-        console.log("button pressed")
-        navigation.navigate('Image')
-      }
-      }
-      title="Go to Image screen" />
+      <Button
+        onPress={() => {
+          console.log('button pressed')
+          navigation.navigate('Image')
+        }}
+        title="Go to Image screen"
+      />
 
-    <Button
-      onPress={() => {
-        navigation.navigate('Counter')
-      }
-      }
-      title="Go to Counter screen" />
-    <Button
-      onPress={() => {
-        navigation.navigate('Color')
-      }
-      }
-      title="Go to Color screen" />
+      <Button
+        onPress={() => {
+          navigation.navigate('Counter')
+        }}
+        title="Go to Counter screen"
+      />
+      <Button
+        onPress={() => {
+          navigation.navigate('Color')
+        }}
+        title="Go to Color screen"
+      />
 
-    <Button
-      onPress={() => {
-        navigation.navigate('Square')
-      }
-      }
-      title="Go to Square screen" />
-  </View>
-};
+      <Button
+        onPress={() => {
+          navigation.navigate('Square')
+        }}
+        title="Go to Square screen"
+      />
+      <Button
+        onPress={() => {
+          navigation.navigate('Text')
+        }}
+        title="Go to Text screen"
+      />
+    </View>
+  )
+}
 
 const styles = StyleSheet.create({
   text: {
     fontSize: 30,
   },
-});
+})
 
-export default HomeScreen;
-
-
+export default HomeScreen
