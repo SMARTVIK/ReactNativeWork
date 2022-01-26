bad402c939e3d899f7b516af155d21b038d4e23a
smartvik <vps631532@gmail.com>
2022-01-27 00:54:27 +0530

    Exercise to checking password length is done

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,27 +1,32 @@
-import React, {useState} from 'react';
-import {View, StyleSheet, Text, TextInput} from 'react-native';
-
+import React, { useState } from 'react'
+import { View, StyleSheet, Text, TextInput } from 'react-native'
 
 const TextScreen = () => {
-    const [name,setName] = useState('');
-    return <View>
-        <Text>Enter Name:</Text>
-        <TextInput 
+  const [name, setName] = useState('')
+  return (
+    <View>
+      <Text>Enter Password:</Text>
+      <TextInput
         style={styles.input}
         autoCapitalize="none"
         autoCorrect={false}
         value={name}
-        onChangeText={(newValue) => {setName(newValue)}}/>
-        <Text>My Name is {name}</Text>
+        onChangeText={(newValue) => {
+          setName(newValue)
+        }}
+      />
+    
+      {name.length >=5 ? null : <Text>Password must be greater than 5 characters.</Text>}
     </View>
+  )
 }
 
 const styles = StyleSheet.create({
-    input:{
-        margin: 15,
-        borderColor: 'black',
-        borderWidth: 1
-    }
-});
+  input: {
+    margin: 15,
+    borderColor: 'black',
+    borderWidth: 1,
+  },
+})
 
-export default TextScreen;
+export default TextScreen
